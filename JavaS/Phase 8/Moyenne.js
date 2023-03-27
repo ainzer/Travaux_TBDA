92da5b0fa62c2193a6370945417505ae296d5e61
ainzer <tony.spicher@hotmail.com>
2023-03-27 16:31:33 +0200

    Changement

--- JavaS/Phase 8/Moyenne.js
+++ JavaS/Phase 8/Moyenne.js
@@ -1,18 +1,3 @@
-/*var i = 0;
-var somme = 0;
-var moyenne = 0;
-var nb_notes = 0;
-nb_notes = prompt("Souhaitez-vous faire la moyenne de combien de notes ?", "Nb de Notes");
-nb_notes = parseInt(nb_notes);
-notes = new Array(nb_notes);
-for (i = 1; i <= nb_notes; i++) {
-
-    note_i = prompt("Entrez la note");
-    notes[i - 1] = parseInt(note_i);
-    somme += notes[i - 1];
-}
-moyenne = (somme / nb_notes)
-alert("La moyenne des notes est : " + moyenne);*/
 
 
 
