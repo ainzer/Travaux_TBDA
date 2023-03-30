176d6ab58868edbf0c364736affcc83bf0e26f72
ainzer <tony.spicher@hotmail.com>
2023-03-30 12:34:20 +0200

    changement

--- JavaS/Phase 8/Saisie.js
+++ JavaS/Phase 8/Saisie.js
@@ -1,9 +1,10 @@
 var nbnom = 0;
 do {
     nbnom++;
-    var prenom = window.prompt("Saisissez lz prénom N°" + nbnom + "\n" + "ou Clic sur Annuler pour arrêter la saisie");
+    var prenom = window.prompt("Saisissez le prénom N°" + nbnom + "\n" + "Laisser le champ vide et appuyer sur entrer");
     nbnom = parseInt(nbnom);
     var i = prenom;
 } while (i != false);
 nbnom--;
 alert("Le nombre de prénom est : " + nbnom);
+location.reload();
