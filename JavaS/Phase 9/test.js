17f9231f403b6f1c31dc8ab0af6f4ab3de94452a
ainzer <tony.spicher@hotmail.com>
2023-03-31 13:10:17 +0200

    changement

--- JavaS/Phase 9/test.js
+++ JavaS/Phase 9/test.js
@@ -1,15 +1,4 @@
-var nb_1 = prompt("Entrez un nombre : ");
-var nb_2 = prompt("Entrez un multiplicateur");
-
-
-function produit (x, y) 
-{
-    //alert("Le produit de " + x + " * " + y + " est égal à " + result);
-    var result = nb_1 * nb_2;
+// Définition de la fonction produit
+function produit (x, y) {
+    return x * y;
 }
-
-produit(nb_1, nb_2);
-
-document.write("Le produit de " + x + " * " + y + " est égal à " + result)
-
-location.reload();
